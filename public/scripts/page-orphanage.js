48f322d5b0503bde1d60ae8f257aa1b04a7f8877
lucaastorres <lucaas.torres@live.com>
2020-10-21 15:02:56 -0300

    day2

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -26,4 +26,14 @@ L
 function selectImage(event) {
     const button = event.currentTarget
     
+    const buttons = document.querySelectorAll(".images button")
+
+    buttons.forEach(button => button.classList.remove('active'))
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector('.orphanage-details > img')
+
+    imageContainer.src = image.src
+    
+    button.classList.add('active')
 }
