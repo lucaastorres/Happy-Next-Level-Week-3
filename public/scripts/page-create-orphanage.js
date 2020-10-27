28aa81c6023e72e2b48f53dd3f8f10344648901e
lucaastorres <lucaas.torres@live.com>
2020-10-27 19:54:24 -0300

    clear foto

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -14,10 +14,45 @@ let marker
 map.on('click', (event) => {
     const lat = event.latlng.lat
     const lng = event.latlng.lng
-    document.querySelector('[name="lat"')
+    document.querySelector('[name=lat]').value = lat
+    document.querySelector('[name=lng]').value = lng
 
     marker && map.removeLayer(marker)
 
     marker = L.marker([lat, lng], { icon })
     .addTo(map)
 })
+
+function addPhotoField() {
+
+    const container = document.querySelector('#images')
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    const input = newFieldContainer.children[0]
+
+    if( input.value == "" ) {
+        return 
+    }
+
+    input.value = ""
+
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length < 2 ) {
+
+        span.parentNode.children[0].value = ""
+        
+        return
+    }
+
+    span.parentNode.remove()
+}
