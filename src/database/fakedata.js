f95708bda508a4a63b207ad79e4159dcf7339ca4
lucaastorres <lucaas.torres@live.com>
2020-10-30 22:05:39 -0300

    day4

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.2216835",
+        lng: "-49.649508",
+        name: "Lar das meninas",
+        descripition: "Presta assistência a crianças de 6 a 15 anos em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1574350518720-d92affb18bee?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1592840330988-3c88e47c1aac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 8h até 18h",
+        open_on_weekends: "1"
+
+    },
+    
+    {
+        id: 2,
+        lat: "-27.2216835",
+        lng: "-49.659508",
+        name: "Lar dos meninos",
+        descripition: "Presta assistência a crianças de 6 a 15 anos em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1574350518720-d92affb18bee?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1592840330988-3c88e47c1aac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 8h até 18h",
+        open_on_weekends: "0"
+    }
+]
--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ server
     .use(express.static('public'))
     .set('views', path.join(__dirname, "views"))
     .set('view engine', 'hbs')
+    
     .get('/', pages.index)
+    .get('/orphanage', pages.orphanage)
+    .get('/orphanages', pages.orphanages)
+    .get('/create-orphanage', pages.createOrphanage)
 
 server.listen(5500)
